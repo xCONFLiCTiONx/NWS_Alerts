2081f436ee174e7e50dc436a55b43803dc8bbe4a
xCONFLiCTiONx <mikehall.outbox@gmail.com>
2020-01-29 16:53:43 -0600

    Release 2.4.0.0

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
